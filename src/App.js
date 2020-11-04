c363873e2319a2bcc6a977c18919afd8541a7260
Ethan Hansen <hanseneg@hotmail.com>
2020-11-04 12:16:47 -0700

    commit5

--- src/App.js
+++ src/App.js
@@ -8,8 +8,22 @@ class App extends React.Component {
          this.state = {
              colors: ['white', 'white', 'white', 'white'],
         //     shape: [square, square, square, square],
-        //     text: [white, white, white, white]
+             text: ['square1', 'square2', 'square3', 'square4']
          }
+         this.hndlBtn1 = this.hndlBtn1.bind(this)
+    }
+    hndlBtn1(){
+        this.setState(prevState => {
+            if(prevState.colors === 'white'){
+                return {
+                    colors: ['black', 'black', 'black', 'black']
+                }
+            }else {
+                return {
+                    colors: ['white', 'white', 'white', 'white']
+                }
+            }
+        })
     }
     
     render() {
@@ -25,6 +39,8 @@ class App extends React.Component {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
+            borderStyle: 'solid',
+            borderColor: 'orange'
         }
         const topRight = {
             gridColumn: '2',
@@ -52,21 +68,25 @@ class App extends React.Component {
                     <div style={topLeft}>
                         <Squares 
                         color={this.state.colors[0]}
+                        text={this.state.text[0]}
                         />
                     </div>
                     <div style={topRight}>
                         <Squares 
                         color={this.state.colors[1]}
+                        text={this.state.text[1]}
                         />
                     </div>
                     <div style={bottomLeft}>
                         <Squares 
                         color={this.state.colors[2]}
+                        text={this.state.text[2]}
                         />
                     </div>
                     <div style={bottomRight}>
                         <Squares 
                         color={this.state.colors[3]}
+                        text={this.state.text[3]}
                         />
                     </div>
                 </div>
--- src/Squares.js
+++ src/Squares.js
@@ -3,15 +3,14 @@ import React from 'react'
 class Squares extends React.Component {
     render() {
         const square = {
-            color: 'green',
-            backgroundColor: 'blue',
+            // color: 'green',
+            // backgroundColor: 'blue',
             justifyContent: 'center',
             alignItems: 'center',
             textAlign: 'center'
         }
         return(
             <div style={square}>
-                Square
                 {this.props.color}
                 {this.props.shape}
                 {this.props.text}
